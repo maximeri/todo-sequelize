67ecf8d1ba27c8649489f6e7139df169bb662397
maximeri <t.s.eliot1996@gmail.com>
2022-04-15 11:49:41 +0800

    feat: add facebook login feature

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
@@ -21,13 +22,44 @@ module.exports = app => {
       })
       .catch(err => done(err, false))
   }))
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRECT,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName'],
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const { name, email } = profile._json
+          let user = await User.findOne({ where: { email } })
+          if (user) return done(null, user)
+
+          const randomPassword = Math.random().toString(36).slice(-8)
+          const salt = await bcrypt.genSalt(10)
+          const hash = await bcrypt.hash(randomPassword, salt)
+          // for new user creation
+          user = await User.create({
+            name,
+            email,
+            password: hash,
+          })
+          return done(null, user)
+        } catch (err) {
+          return done(null, false)
+        }
+      }
+    )
+  )
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
   passport.deserializeUser((id, done) => {
     User.findByPk(id)
       .then((user) => {
-        user = user.toJSON()
+        user = user.toJSON() // 此 user 物件會透過 req.user 傳到前端樣板，要先轉成 plain object，否則無法顯示
         done(null, user)
       }).catch(err => done(err, null))
   })
