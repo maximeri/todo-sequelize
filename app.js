ff81a747828a297eb8c86539055bf68f14fa5f86
maximeri <t.s.eliot1996@gmail.com>
2022-04-15 11:49:54 +0800

    feat: add .env

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+// dotenv start
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 const routes = require('./routes')
 const session = require('express-session')
 const usePassport = require('./config/passport')
@@ -12,7 +16,7 @@ app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
@@ -30,4 +34,5 @@ app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
+  // console.log(process.env)
 })
