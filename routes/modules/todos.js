9f7c96d6cedad2b356021fd89ab35f7b20a59ddb
maximeri <t.s.eliot1996@gmail.com>
2022-04-16 19:18:10 +0800

    fix: delete function and detail view

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -23,7 +23,7 @@ router.get('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
   return Todo.findOne({ where: { id, UserId } })
-    .then(todo => res.render('detail', { todo:todo.toJSON }))
+    .then(todo => res.render('detail', { todo:todo.toJSON() }))
     .catch(error => console.log(error))
 })
 
@@ -32,7 +32,7 @@ router.get('/:id/edit', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
   return Todo.findOne({ where: { id, UserId } })
-    .then(todo => res.render('edit', { todo: todo.get() }))
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
 
@@ -54,7 +54,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req,res)=>{
   const UserId = req.user.id
   const id = req.params.id
-  return Todo.findOne({ UserId, id})
+  return Todo.findOne({ where: { id, UserId } })
     .then(todo => todo.destroy())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
